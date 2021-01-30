d9991dab4534fe43102f9fb1e6e350608a7d4205
Júnior Oliveira <juninsoader@gmail.com>
2021-01-30 14:43:58 -0300

    Category

---
+++ src/main/java/com/jr/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.jr.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jr.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+}
